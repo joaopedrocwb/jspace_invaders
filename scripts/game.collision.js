a0b9e6f4ad8b8739958426e373a550db6356ba25
Iraê <irae@irae.pro.br>
2010-01-29 05:32:37 -0200

    First collision detection commit with unit test suite.

--- scripts/game.collision.js
+++ scripts/game.collision.js
@@ -1,16 +1,14 @@
-// 
 
-
-var GameCollision;
 (function(){
 	
-	var objects = [];
 	var settings = {
 		offsetY : 0,
 		offsetX : 0
 	}
 	
 	function testCollision(test) {
+		// console.info(this.objects)
+		return false;
 		var offX=settings.offsetX,
 			offY=settings.offsetY,
 			collided=true,
@@ -18,26 +16,54 @@ var GameCollision;
 			len = objects.length,
 			i=len;
 		for(curr=objects[--i];i>=0;){						
-			if (test.y + test.h - (offY * 2) < curr.y + offY) ||
+			if (
+				(test.y + test.h - (offY * 2) < curr.y + offY) ||
 				(test.y + offY > curr.y + curr.h - (offY * 2)) ||
 				(test.x + test.w - (offX * 2) < curr.x + offX) ||
 				(test.x + offX > curr.x + curr.w - (offX * 2))
-			{
-					collided = false;
+				) {
+				collided = false;
+			} else {
+				return curr;
 			}
 		}
+		return false;
 	};
 	
-	GameCollision = {
-		create:function GameCollisionCreate (obj) {
-			
+	function isValidObject(obj) {
+		return obj.rect && typeof obj.rect.x == 'number' && typeof obj.rect.y == 'number' && typeof obj.rect.h == 'number' && typeof obj.rect.w == 'number';
+	};
+	
+	function GameCollision() {
+		// avoid problems when not called preceeded by `new`
+		if(!this.add) {
+			return new GameCollision();
+		}
+		this.objects = [];
+		this.timestamp = ''+(new Date()).getTime();
+		return this;
+	};
+	
+	// public methods
+	GameCollision.prototype = {
+		add: function(obj){
+			if(isValidObject(obj)) {
+				this.objects.push(obj);
+				testCollision.call(this,obj);
+				return true;
+			} else {
+				throw "GameCollision.create(obj) obj must be like {rect:{x:0,y:0,h:1,w:1}}";
+				return false;
+			}
 		},
-		remove:function GameCollisionRemove(obj) {
-			
-		},
-		update:function GameCollisionUpdate(obj) {
+		remove: function(obj){},
+		update: function(obj){
 			
 		}
 	};
 	
+	// export public constructor
+	window.GameCollision = GameCollision;
+
 })();
+
---
+++ test/collision.unit.js
@@ -0,0 +1,16 @@
+module("collision");
+test("instances", function() {
+	expect(3);
+	ok(!!GameCollision,'exists')
+	var collision_a = new GameCollision();
+	var collision_b = GameCollision();
+	ok(collision_a instanceof GameCollision, 'init with new');
+	ok(collision_b instanceof GameCollision, 'init without new');
+});
+
+test("add/remove objects",function(){
+	expect(1);
+	var collision_a = new GameCollision();
+	var ob1 = {rect:{x:10,y:10,h:10,w:10}};
+	ok(collision_a.add(ob1));
+});
