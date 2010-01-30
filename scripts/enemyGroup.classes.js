706cc6ecb2536f6e222a5ae61d96030967f4a179
unknown <igor@.(none)>
2010-01-30 20:00:42 -0200

    Movimento em andamento

--- scripts/enemyGroup.classes.js
+++ scripts/enemyGroup.classes.js
@@ -1,7 +1,22 @@
 var EnemyGroup = new Class({
-    "initialize": function(cols, rows, distance) {
+    "initialize": function(cols, rows, distance, stage) {
 		this.create(cols, rows, distance);
 		this.totalEnemys = cols*rows;
+		this.goto = 'right';
+		this.stageWidth = 674;
+		
+		this.setPosition(cols, rows, distance);
+		
+		this.speed = 1000 * Math.cos(stage);
+		
+		alert(this.speed);
+		
+		/*var that = this;
+		setTimeout(function()
+		{
+			this.move.call(that);
+		}, 50);*/
+		this.move();
     },
     
     "position": function() {
@@ -41,5 +56,56 @@ var EnemyGroup = new Class({
 	"destroy": function()
 	{
 		this.object.destroy();
+	},
+	
+	"setPosition": function(cols, rows, distance)
+	{
+		var dif = distance - 30;
+		
+		this.objWidth = cols * distance - dif;
+		this.objHeight = rows * distance - dif;
+		this.top = 0;
+		this.left = this.stageWidth/2 - this.objWidth / 2;
+		
+		this.object.setStyles(
+		{
+			"width": this.objWidth,
+			"height": this.objHeight,
+			"left": this.left
+		});
+	},
+		
+	"move": function()
+	{
+		if(this.goto = "right")
+		{
+			this.top += 3;
+			this.left += 10;
+		}
+		else
+		{
+			this.top -= 3;
+			this.left -= 10;
+		}
+		
+		if(this.left <= 0)
+		{
+			this.left = 0;
+			this.goto = "right";
+		}
+		
+		if(this.left >= this.stageWidth - this.objWidth)
+		{
+			this.left = this.stageWidth - this.objWidth;
+			this.goto = "left";
+		}
+		
+		
+		this.object.setStyles({
+			"top": this.top,
+			"left": this.left
+		});
+		
+		setTimeout(this.move.bind(this), this.speed);
 	}
 });
--- scripts/gameStages.classes.js
+++ scripts/gameStages.classes.js
@@ -14,29 +14,6 @@ var GameStages = new Class({
 	{
 		if(typeof enemyGroup != "undefined") enemyGroup.destroy();
 		
-		enemyGroup = new EnemyGroup(8, 3, 50);
-		this.setPosition(8, 3, 50);
-	},
-	
-	"setPosition": function(cols, rows, distance)
-	{
-		dif = distance - 30;
-		
-		obj = enemyGroup.object;
-		objWidth = cols * distance - dif;
-		objHeight = rows * distance - dif;
-		
-		obj.setStyles(
-		{
-			"width": objWidth,
-			"height": objHeight,
-			"top": objHeight * -1, 
-			"left": 674/2 - objWidth / 2
-		});
-	},
-	
-	"move": function()
-	{
-		
+		enemyGroup = new EnemyGroup(8, 3, 50, this.stage);
 	}
 });
